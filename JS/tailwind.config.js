4fbd83a07361d955875d19ae1d55deacc423479f
hasanariful <arifulhasan90r@gmail.com>
2023-10-09 23:29:44 +0600

    first commit

---
+++ JS/tailwind.config.js
@@ -0,0 +1,55 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        // My code start here
+        // Color code start
+        colors: {
+          headercolor: 'rgba(22, 12, 109, 0.50)',
+          hovercolor: '#1BBF00',
+          overlycolor: 'rgba(106, 98, 197, 0.90)',
+          countertextcolor: '#141135',
+          readcolor: "rgba(25, 191, 0, 0.3)",
+          aftercolor: "rgba(79, 88, 95, 0.2)",
+          afterhovercolor: "rgba(255, 255, 255, 0.3)",
+          choosetextcolor: "#726E9E",
+          dotextcolor: "#6C7D93",
+          docardcolor: "#6B62C5",
+          cardpcolor: "#E6E5F3",
+          
+        },
+        // Color code end
+        
+        //Background image part
+        backgroundImage: {
+          bannerimg: "url('Images/Bannerimg.jpg')",
+          counterimg: "url('Images/Counter.png')",
+          choosebg: "url('Images/choosebg.png')",
+          dobg: "url('Images/dobg.png')",          
+        },
+        //Background image part
+
+        //Padding Part 
+        
+        //Padding Part 
+
+        //Font Family
+        fontFamily: {
+          open: ['Open Sans', 'sans-serif'],
+          papri: ['Paprika ', 'cursive'],
+        },
+        //Font Family
+
+        //All Containers
+        maxWidth: {
+          headercontainer: '1170px',
+          bannertextcontainer: '962px',
+         },
+        //All Containers
+
+
+
+        // My code end here
+
+      }
+    }
+  }
